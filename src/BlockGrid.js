48afe62e060ca422a7f50907ed9f980519519c01
chloe montgomery <chloe.montgomery@capgemini.com>
2019-10-15 15:23:11 +0100

    Final solution

--- src/BlockGrid.js
+++ src/BlockGrid.js
@@ -13,46 +13,57 @@ class BlockGrid {
       for (let y = 0; y < this.height; y++) {
         col.push(new Block(x, y));
       }
-
       this.grid.push(col);
     }
   }
 
   render(el = document.getElementById('gridEl')) {
-    for (let x = 0; x < this.width; x++) {                      // for each column
-      const id = 'col_' + x;                                    // create a column id 
-      const colEl = document.createElement('div');              // create a new div and variable name for it
-      colEl.id = id;                                            // give the new div the id we made
-      colEl.className = 'col';                                  // give the new div a classname 
-      el.appendChild(colEl);                                    // append the new div to the main grid element
+    for (let x = 0; x < this.width; x++) {
+      const id = 'col_' + x;
+      const colEl = document.createElement('div');
+      colEl.id = id;
+      colEl.className = 'col';
+      el.appendChild(colEl);
+      this.renderBlocks(x, colEl)
+    }
+  }
 
-      for (let y = this.height - 1; y >= 0; y--) {              // for each row
-        const block = this.grid[x][y];                          // find the block from the grid made in constructor -- 0,0 0,1 0,2 etc... then 1,0 1,1 ...
-        const id = `block_${x}x${y}`;                           // create an id for block
-        const blockEl = document.createElement('div');          // create a new div for block
-
-        blockEl.id = id;                                        // assign block the id we created
-        blockEl.className = 'block';                            // assign block the classname block
-        blockEl.style.background = block.colour;                // set the colour of the div to the colour prop of the block
-        blockEl.addEventListener('click', evt => this.blockClicked(evt, block));    // ass event listener to block
-        colEl.appendChild(blockEl);                             // append block to column
+  reRenderColumns() {
+    for (let x = 0; x < this.width; x++) {
+      let column = document.getElementById(`col_${x}`)
+      if (column) {
+        while (column.firstChild) {
+          column.removeChild(column.firstChild);
+        }
+        this.renderBlocks(x, column)
       }
+    }
+  }
+
+  renderBlocks(x, column) {
+    for (let y = this.height - 1; y >= 0; y--) {              
+      const block = this.grid[x][y];
+      const id = `block_${x}x${y}`;                           
+      const blockEl = document.createElement('div');          
+      blockEl.id = id;                                      
+      blockEl.className = 'block';                            
+      blockEl.style.background = block ? block.colour : 'white';                
+      blockEl.addEventListener('click', evt => this.blockClicked(evt, block));    
+      column.appendChild(blockEl);                                                    
     }
   }
 
   blockClicked(e, block) {
     block.markedForDelete()
 
-    // get the co-ordinates of the block clicked 
     let x = block.x
     let y = block.y
     let selectedBlockColour = block.colour
     
     this.markBlocks(x, y, selectedBlockColour)
-
     this.removeMarkedBlocks()
-
     this.updateCoOrdinates()
+    this.reRenderColumns()
   }
 
   markBlocks(x, y, colour) {
@@ -84,7 +95,6 @@ class BlockGrid {
       })
     }
   }
-
 }
 
 export default BlockGrid;
